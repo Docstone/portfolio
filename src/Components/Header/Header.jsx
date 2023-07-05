b8c39367e30b8a49fa6413727276620e1dd922b0
Docstone <76525551+Docstone@users.noreply.github.com>
2023-07-05 16:00:35 +0200

    Text Intro

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,8 +11,8 @@ export const Header = () => {
                 <h1>Bonjour! je suis</h1>
                 <h2>Matthieu Da Rocha</h2>
                 <h3><span>Dévelopeur Web Fullstack</span> Junior basé à Paris</h3>
-                <p>Passionné des technologies d'information et programation j'ai entrepris une reconversion depuis le monde de la gastronomie a celui du dévelopement web</p>
-                <p>Diplomé de la formation d'OpenClassrooms (Dévelopeur Web, RNCP 5) durant laquelle j'ai réaliser les projets que vous retrouverez dans ce portfolio.</p>
+                <p>Passionné par les technologies de l'information et la programmation, j'ai récemment opéré une reconversion depuis le monde de la gastronomie vers le développement web.</p>
+                <p>Diplômé de la formation de Développeur Web d'OpenClassrooms (RNCP 5), j'ai réalisé divers projets que vous retrouverez dans ce portfolio. Mon objectif est de continuer à développer mes compétences et de contribuer à des projets innovants. N'hésitez pas à explorer mes réalisations et à me contacter pour des opportunités de collaboration.</p>
             </div>
             <div className="faceWrap">
                 <img src={face} alt="my face" />
